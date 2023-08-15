f398b45992445cbbe5d02399a70aee3af6048ad1
Mikhail Kalinin <noblesse.knight@gmail.com>
2023-08-15 16:29:08 +0600

    Update EB processing on epoch boundary

--- specs/_features/maxeb_increase/capella.py
+++ specs/_features/maxeb_increase/capella.py
@@ -1634,11 +1634,14 @@ def process_effective_balance_updates(state: BeaconState) -> None:
         HYSTERESIS_INCREMENT = uint64(EFFECTIVE_BALANCE_INCREMENT // HYSTERESIS_QUOTIENT)
         DOWNWARD_THRESHOLD = HYSTERESIS_INCREMENT * HYSTERESIS_DOWNWARD_MULTIPLIER
         UPWARD_THRESHOLD = HYSTERESIS_INCREMENT * HYSTERESIS_UPWARD_MULTIPLIER
+        EFFECTIVE_BALANCE_LIMIT = MIN_ACTIVATION_BALANCE
+        if has_compounding_withdrawal_credential(validator):
+            EFFECTIVE_BALANCE_LIMIT = MAX_EFFECTIVE_BALANCE
         if (
             balance + DOWNWARD_THRESHOLD < validator.effective_balance
             or validator.effective_balance + UPWARD_THRESHOLD < balance
         ):
-            validator.effective_balance = min(balance - balance % EFFECTIVE_BALANCE_INCREMENT, MAX_EFFECTIVE_BALANCE)
+            validator.effective_balance = min(balance - balance % EFFECTIVE_BALANCE_INCREMENT, EFFECTIVE_BALANCE_LIMIT)
 
 
 def process_slashings_reset(state: BeaconState) -> None:
