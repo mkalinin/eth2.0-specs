d1d7faa6f4a2a033d8a743a9acf10d840aa32e2f
Mikhail Kalinin <noblesse.knight@gmail.com>
2025-11-05 14:38:50 +0500

    More refactor

--- tests/core/pyspec/eth2spec/test/helpers/fast_confirmation_rule.py
+++ tests/core/pyspec/eth2spec/test/helpers/fast_confirmation_rule.py
@@ -1,17 +1,18 @@
 from eth_utils import encode_hex
 
 
-def on_slot_after_attestations_applied_and_append_step(spec, store, test_steps):
-    spec.on_slot_after_attestations_applied(store)
-    test_steps.append({"slot_after_attestations_applied": spec.get_current_slot(store)})
-    checks = {
-        "time": int(store.time),
+def output_fcr_checks(spec, store, test_steps):
+    test_steps.append({
         "prev_epoch_unrealized_justified_checkpoint": {
             "epoch": int(store.prev_epoch_unrealized_justified_checkpoint.epoch),
             "root": encode_hex(store.prev_epoch_unrealized_justified_checkpoint.root),
         },
         "prev_slot_head": encode_hex(store.prev_slot_head),
         "confirmed_root": encode_hex(store.confirmed_root),
-    }
+    })
 
-    test_steps.append({"checks": checks})
+
+def on_slot_after_attestations_applied_and_append_step(spec, store, test_steps):
+    spec.on_slot_after_attestations_applied(store)
+    test_steps.append({"slot_after_attestations_applied": spec.get_current_slot(store)})
+    output_fcr_checks(spec, store, test_steps)
